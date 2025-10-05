a1e9576a1f3c6291c5ec86993440d994f009949b
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-05 10:25:14 +0500

    changes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-3xl '>
+        My name is fahad Bin javed
+        </div>
+    </>
+  )
+}
+
+export default App
